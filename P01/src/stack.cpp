6afb340c58c5faf9d3c7de121ea91faf0f83fa4a
Tikualdo <alu0101571836@ull.edu.es>
2025-09-24 16:18:52 +0100

    Primera implementación de la clase Stack

--- P01/src/stack.cpp
+++ P01/src/stack.cpp
@@ -12,9 +12,19 @@
   * Historial de revisiones
   *     // 
 */
-#include <iostream>
 #include "../include/stack.hpp"
 
-int main(int argc, char* argv[]) {
+template <typename T>
+void Stack<T>::push(const T& element) {
+  elements.push_back(element);
+}
 
+template <typename T>
+T Stack<T>::pop() {
+  if (elements.empty()) {
+    throw std::out_of_range("Stack<>::pop(): empty stack");
+  }
+  T elem = elements.back();
+  elements.pop_back();
+  return elem;
 }
